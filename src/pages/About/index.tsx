50ba99485f0d107ba9c59b155144204120a6660d
JonathanLemes <JonathanLemes@unifei.edu.br>
2021-01-17 19:18:05 -0300

    style(about): cSS adapted to phones

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -15,19 +15,19 @@ export default function About() {
             <p className="nome">CAIQUE CORREA</p>
         </div>
         <div className="content-block">
-            <Link to="/" className="btn"><img src={ImgHome} alt="Home" /></Link>
+            <Link to="/" className="btn home"><img src={ImgHome} alt="Home" /></Link>
             <div className="about-block">
                 <span className="text">
                 {`Meu nome é Caique, sou um estudante de Psicologia apaixonado por metodologia científica e pela área acadêmica (Pesquisa).
 
-                Busco frequentemente me informar sobre atualizações de normas técnicas (ABNT/APA) e realizar cursos que me permitam aprimorar meus conhecimentos de metodologia para então transmiti-los, tendo em vista o objetivo de prestar contribuições para o avanço da pesquisa científica no Brasil.
+                Busco frequentemente me informar sobre atualizações de normas técnicas (ABNT/APA) e realizar cursos que me permitam aprimorar meus conhecimentos de metodologia científica para então transmiti-los, tendo em vista o objetivo de prestar contribuições para o avanço da pesquisa científica no Brasil.
 
                 Atualmente estou no 4° ano de Psicologia e desde o meu segundo ano da graduação estou envolvido na condução de pesquisas e projetos na área de Educação e Saúde e, durante esse tempo, acumulei grandes experiências e desenvolvi competências que me permitem hoje elaborar trabalhos acadêmicos com maior qualidade.
 
                 Atuei em um dos importantes núcleos de Ensino, Pesquisa e Extensão do UNISAL/Lorena nomeado “Observatório de Violências nas Escolas”, filiado à UNESCO e ao CNPq, tanto como voluntário quanto bolsista de Iniciação Científica e Extensão, desenvolvendo pesquisas e projetos, auxiliando na formatação destes, tirando dúvidas sobre metodologia científica, submissão de projetos ao Comitê de Ética em Pesquisa, formatando editais, e claro, aprendendo com profissionais de renome a como desenvolver trabalhos de excelência.`}
                 </span>
             </div>
-            <Link to="/experiencias" className="btn"><img src={ImgExperiencias} alt="Experiências acadêmicas" /></Link>
+            <Link to="/experiencias" className="btn experiencias"><img src={ImgExperiencias} alt="Experiências acadêmicas" /></Link>
         </div>
     </div>
   );
