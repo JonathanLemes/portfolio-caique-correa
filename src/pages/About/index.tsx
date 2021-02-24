0b10aebfb028c676531b2b2353b4b505fa880744
JonathanLemes <JonathanLemes@unifei.edu.br>
2021-02-24 11:13:27 -0300

    refactor(about): about text changed

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -24,7 +24,9 @@ export default function About() {
 
                 Atualmente estou no 4° ano de Psicologia e desde o meu segundo ano da graduação estou envolvido na condução de pesquisas e projetos na área de Educação e Saúde e, durante esse tempo, acumulei grandes experiências e desenvolvi competências que me permitem hoje elaborar trabalhos acadêmicos com maior qualidade.
 
-                Atuei em um dos importantes núcleos de Ensino, Pesquisa e Extensão do UNISAL/Lorena nomeado “Observatório de Violências nas Escolas”, filiado à UNESCO e ao CNPq, tanto como voluntário quanto bolsista de Iniciação Científica e Extensão, desenvolvendo pesquisas e projetos, auxiliando na formatação destes, tirando dúvidas sobre metodologia científica, submissão de projetos ao Comitê de Ética em Pesquisa, formatando editais, e claro, aprendendo com profissionais de renome a como desenvolver trabalhos de excelência.`}
+                Atuei em um dos importantes núcleos de Ensino, Pesquisa e Extensão do UNISAL/Lorena nomeado “Observatório de Violências nas Escolas”, filiado à UNESCO e ao CNPq, tanto como voluntário quanto bolsista de Iniciação Científica e Extensão, desenvolvendo pesquisas e projetos, auxiliando na formatação destes, tirando dúvidas sobre metodologia científica, submissão de projetos ao Comitê de Ética em Pesquisa, formatando editais, e claro, aprendendo com profissionais de renome a como desenvolver trabalhos de excelência.
+
+                Hoje, continuo a desenvolver pesquisas tanto fomentadas por Programas de Concessão de Bolsas quanto de maneira voluntária mediante colaboração com outros profissionais e companheiras de graduação, tendo em vista cada vez mais me aprimorar não somente como estudante e profissional em formação, mas também como ser humano, ético, crítico e implicado com a realidade e com o compromisso social com a vida humana.`}
                 </span>
             </div>
             <Link to="/experiencias" className="btn experiencias"><img src={ImgExperiencias} alt="Experiências acadêmicas" /></Link>
